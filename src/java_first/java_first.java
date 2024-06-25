e6bbd49a54064d12c99ad7681eaf2476951ceac2
hyjun <hyjun@seo_hj>
2024-06-25 22:56:35 +0900

    asdfasdf

--- src/java_first/java_first.java
+++ src/java_first/java_first.java
@@ -3,6 +3,8 @@ package java_first;
 public class java_first {
 	public static void main(String[] args) {
 		System.out.println(" 자바 테스트");
+		System.out.println("java test");
+		System.out.println("java");
 	}
 
 
